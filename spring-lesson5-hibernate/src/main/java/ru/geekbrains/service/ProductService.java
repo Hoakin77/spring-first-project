ba25bd81262b23b6a6f8078c1ea3c30924998089
Munir Khasanov <Khasanov_munir@mail.ru>
2022-04-28 14:23:07 +0300

    домашнее задание 5

---
+++ spring-lesson5-hibernate/src/main/java/ru/geekbrains/service/ProductService.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.service;
+
+import ru.geekbrains.persistence.model.Product;
+
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProductList();
+
+    void saveOrUpdate(Product product);
+
+   Product getProductById(Long id);
+
+    void deleteById(Long id);
+
+}
